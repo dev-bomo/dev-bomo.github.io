55931c551b61cc34ede96d340e5f4636ebdbd689
Bogdan Moldovean <bogdanmoldovean@Bogdans-MacBook-Pro.local>
2019-10-22 09:57:54 +0300

    changed some of the text so we have preorders instead of beta testing

--- js/i18n.js
+++ js/i18n.js
@@ -1,16 +1,16 @@
 // import i18next from 'i18next';
 
-window.a_lng = 'ro';
+window.a_lng = 'en';
 
 i18next.init({
-    lng: 'ro',
+    lng: 'en',
     debug: true,
     resources: {
         en: {
             translation: {
-                "lang": "English",
-                "register_button": "Register for beta",
-                "subheader_text": "Want a greener home? Are you excited by tech? Be on the forefront of the home gardening revolution by taking part in the Arbour beta testing program.",
+                "lang": "Romana",
+                "register_button": "Preorder",
+                "subheader_text": "Want a greener home? Are you excited by tech? Be on the forefront of the home gardening revolution by preordering the Meridia Arbour.",
                 "what_is": "What is Arbour",
                 "arbour_title": "The Meridia Arbour",
                 "arbour_subtitle": "An Internet of Things plant growing system for indoors and small spaces",
@@ -22,12 +22,9 @@ i18next.init({
                 "details_works": "How it works",
                 "details_works_2": "Press a button on your phone to start watering or set a schedule and see how your plants are doing",
                 "register_title": "Welcome, early adopter!",
-                "register_p1": "Because we want to build a great product and because we want your input on how to make it great we're doing an open beta for the Meridia Arbour. This is your chance to help build the next big thing in home gardening.",
-                "register_p2": "By registering for the open beta you get one of the 50 pre-production Arbour devices at a steep discount. We'll also keep in touch so you can give us feedback on what you like, what you don't like and what you think we can improve to make the product better.",
-                "register_p4": "This is just the beginning. Meridia aims to not only make home gardening easier but to also teach people how to grow. We're building a community that can share knowledge and success so we all can live in a greener world. Stay tuned for free app updates to see where this is going and tell us what you think, or get involved and be one of the community drivers.",
-                "register_p3": "The price for the beta testing unit is 250RON (55EUR). This includes hardware + app and any software updates are free.",
-                "register_more": "More details here",
-                "register_gdpr": "We securely store your email and we'll only use it to communicate beta testing related information to you.",
+                "register_p1": `Here at Meridia we're trying to make indoor plant growing a bit more fun and easy. If you think that's something you can get behind, we'd love to know you're interested.`,
+                "register_p2": "We'll keep you up to date with the development process and when it's time to go, you'll be the first to know",
+                "register_gdpr": "We securely store your email and we'll only use it to communicate product related information to you.",
                 "footer_more": "Find out more",
                 "footer_sub_meridia": "Internet of Things plant growing systems for indoors and small spaces",
                 "footer_in_touch": "Keep in touch",
@@ -36,7 +33,7 @@ i18next.init({
                 "caption_0": "The Arbour is designed to fit in a well lit corner of your house or balcony. It is 120cm tall with a footprint of 40cm x 40cm. All you need is to plug it into a power outlet",
                 "caption_1": "With a 10L reservoir and a water recirculating mechanism you can be sure that you won't need to interact with the tower for at leasst 2 weeks.",
                 "caption_2": "The standard pot holder diameter is 12cm and there's a distance of 15cm to the water nozzle. This allows for standard sprout pots. But the pot holders can be replaced with larger ones if needed. Shoot us an email if you know your needs.",
-                "caption_3": "The Wemos D1 mini is the most used Wifi chip based on the Arduino platform. Connect to the server through your home wifi network in just a few seconds and get a notification on the Meridia app any time there's a connection problem. Which will never happen. The chip is very stable.",
+                "caption_3": "The ESP8266 is the most used Wifi chip based on the Arduino platform. Connect to the server through your home wifi network in just a few seconds and get a notification on the Meridia app any time there's a connection problem. Which will never happen. The chip is very stable.",
                 "caption_4": "2 12V pumps with a 1L/min pumping load. This means they will each be working for less than 10s a day to ensure proper watering to even the most thirsty of plants. 2 pumps also means that each of the 2 levels of the tower can be watered independently so they support different types of plants.",
                 "caption_5": "Plug it in, fill the tank, install the app. That's what it takes to control watering from your phone",
                 "caption_6": "You can set a schedule for each of the 2 levels, or you can water manually. The app also gives you tips on how to change your watering habits based on seasons and weather.",
@@ -44,9 +41,9 @@ i18next.init({
         },
         ro: {
             translation: {
-                "lang": "Romana",
-                "register_button": "Participa la beta",
-                "subheader_text": "Vrei o casa mai verde? Iti place tehonologia? Fii in prima linie a revolutiei home gardening luand parte la programul de beta testing al Meridia Arbour",
+                "lang": "English",
+                "register_button": "Inscriere",
+                "subheader_text": "Vrei o casa mai verde? Iti place tehonologia? Fii in prima linie a revolutiei home gardening luand parte la programul de precomanda al Meridia Arbour",
                 "what_is": "Despre Arbour",
                 "arbour_title": "Meridia Arbour",
                 "arbour_subtitle": "Un sistem Internet of Things pentru cresterea plantelor in spatii mici",
@@ -57,13 +54,10 @@ i18next.init({
                 "details_electronics_2": "Un microcip cu wifi care controleaza 2 pompe, fiecare pompa udand unul din cele 2 nivele ale turnului",
                 "details_works": "Cum functioneaza",
                 "details_works_2": "Apasa un buton pe telefon ca sa incepi programul de udare sau fa-ti un plan de udare automata si vezi daca se potriveste plantelor tale",
-                "register_title": "Bine ai venit in programul beta!",
-                "register_p1": "Pentru ca vrem sa facem un super produs si pentru ca avem nevoie de parerea ta pentru a face asta am dat drumul unui program open beta pentru Meridia Arbour. E o ocazie sa contribui la construirea urmatorului lucru 'mare' in gradinaritul in spatii mici.",
-                "register_p2": "Luand parte la programul open beta poti cumpara unul dintre cele 50 de device-uri pre-productie Meridia Arbour cu un discount semnificativ. O sa tinem legatura ca sa poti sa ne spui ce iti place, ce nu iti place si ce crezi ca putem face ca sa imbunatatim produsul.",
-                "register_p4": "Asta e doar inceputul. Scopul Meridia e nu doar sa faca gradinaritul in spatii mici mai simplu, dar si sa invete lumea cum sa creasca. Construim o comunitate care sa imparta cunostinte si succesul, ca sa traim cu totii intr-o lume mai verde. Fii pe faza la update-urile aplicatiei mobile ca sa vezi in ce directie ne indreptam, sau implica-te in crearea de continut si devino unul din liderii comunitatii.",
-                "register_p3": "Pretul device-ului de beta testing este 250RON (55EUR). Pretul include turnul + aplicatia iar orice upgrade software va fi gratis.",
-                "register_more": "Mai multe detalii aici",
-                "register_gdpr": "Pastram adresa ta de email intr-un loc sigur si o vom folosi doar ca sa te informam despre stadiul proiectului de beta testing.",
+                "register_title": "Bine ai venit!",
+                "register_p1": "Aici la Meridia incercam sa facem crescutul plantelor in apartament mai placut si mai usor. Daca ti se pare interesanta ideea, noua ne-ar place sa stim mai multe despre tine.",
+                "register_p2": "Te tinem la curent cu procesul de dezvoltare si cand e timpul sa livram, vei fi prima persoana anuntata.",
+                "register_gdpr": "Pastram adresa ta de email intr-un loc sigur si o vom folosi doar ca sa te informam despre stadiul proiectului.",
                 "footer_more": "Afla mai multe",
                 "footer_sub_meridia": "Sisteme Internet of Things pentru cresterea plantelor in spatii mici",
                 "footer_in_touch": "Tinem legatura",
@@ -72,7 +66,7 @@ i18next.init({
                 "caption_0": "Arbour e gandit sa incapa intr-un colt luminos al casei sau balconului tau. Are 120cm intaltime si ocupa un spatiu de 40x40cm pe podea. Trebuie doar sa il bagi in priza.",
                 "caption_1": "Un rezervor de 10 litri si un mecanism de recirculare a apei asigura o functionare fara realimentare de cel putin 2 saptamani.",
                 "caption_2": "Diametrul suportului standard de ghiveci e 12cm si pe inaltime e un spatiu de 15cm pana la capul de udare. A fost gandit ca sa acomodeze ghivece standard de rasaduri de 12cm. Dar aceste suporturi pot fi schimbate cu altele mai mari daca e nevoie. Da-ne mai multe detalii despre dimensiunile de care ai nevoie.",
-                "caption_3": "Microcipul Wemos D1 mini este unul din cipurile pe platforma Arduino cele mai folosite. Se conecteaza la server prin wifi-ul de acasa in cateva secunde si primesti notificare pe telefon daca se deconecteaza. Dar asta nu se va intampla. Cipul e foarte stabil :). Totusi vei fi notificat si daca dispare conexiunea la internet.",
+                "caption_3": "Microcipul ESP8266 este unul din cipurile pe platforma Arduino cele mai folosite. Se conecteaza la server prin wifi-ul de acasa in cateva secunde si primesti notificare pe telefon daca se deconecteaza. Dar asta nu se va intampla. Cipul e foarte stabil :). Totusi vei fi notificat si daca dispare conexiunea la internet.",
                 "caption_4": "2 pompe de 12V cu debit de 1L/min. Insesamna ca fiecare pompa va fi pornita pentru mai putin de 10s pe zi. 2 pompe inseamna ca fiecare nivel al turnului va fi udat independent.",
                 "caption_5": "Baga-l in priza, instaleaza aplicatia, umple rezervorul cu apa. Asta e tot ce trebuie sa faci ca sa poti sa controlezi udarea plantelor de pe telefon",
                 "caption_6": "Poti sa setezi un program de udare pe fiecare din cele 2 nivele sau poti sa uzi manual, apasand un buton in aplicatie. Aplicatia iti da si instructiuni cum sa schimbi modul de udare in functie de sezon si meteo.",
